a1f1961f5e4826d5691ba9972993a32f62b26aa0
Noah Quinones <nquinone@ufl.edu>
2018-07-28 18:36:17 -0400

    minor changes

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
 
-app.set('view engine' , ejs)
+app.set('view engine' , 'ejs');
+
+app.get('/', function(req, res){
+  res.render('index');
+})
 
 app.listen(8080, function(){
   console.log('Listening in on port 8080');
