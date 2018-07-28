a8cfe037b12881d5657952d8507cccaa36da6e49
Noah Quinones <nquinone@ufl.edu>
2018-07-28 18:26:46 -0400

    Minor changes

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # TCP-communications
+This is a project for messing around with TCP communications between 3rd party devices and browser. Uses express and node.
---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine' , ejs)
+
+app.listen(8080, function(){
+  console.log('Listening in on port 8080');
+})
